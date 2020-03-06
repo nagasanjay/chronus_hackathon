38f8718e6ac34f6d87fffe0b2f571025d8bb6984
monaaaaaaaaaaaa <mohanarubarajamohan@gmail.com>
2020-03-06 11:50:05 -0800

    hello

--- index.js
+++ index.js
@@ -53,6 +53,21 @@ async function download() {
     data = {mona : data};
     data = JSON.stringify(data);
     //console.log(data);
+    function down(filename, text) {
+      var element = document.createElement('a');
+      element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+      element.setAttribute('download', filename);
+    
+      element.style.display = 'none';
+      document.body.appendChild(element);
+    
+      element.click();
+    
+      document.body.removeChild(element);
+    }
+    
+    // Start file download.
+    await down("jsondata.json",data);
     
 }
 
